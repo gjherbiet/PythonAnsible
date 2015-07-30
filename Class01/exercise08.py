24d073ab5d9f4b4a934bb6975323a2a5682edcb0
Guillaume-Jean Herbiet <gjherbiet@gmail.com>
2015-07-30 22:36:36 +0200

    Impoved exercise 08.

--- Class01/exercise08.py
+++ Class01/exercise08.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python
+'''
+The script should find all of the crypto map entries in the file (lines that
+begin with 'crypto map CRYPTO') and print out the children of each crypto map.
+'''
 
 from ciscoconfparse import CiscoConfParse
 
-# Parse config file
-cfg = CiscoConfParse('cisco_ipsec.txt')
+def main(cisco_file='cisco_ipsec.txt'):
+    '''
+    Find all of the crypto map entries in the file (lines that begin with
+    'crypto map CRYPTO') and print out the children of each crypto map.
+    '''
+    
+    cisco_cfg = CiscoConfParse(cisco_file)
+    
+    for crypto_map in cisco_cfg.find_objects(r'^crypto map CRYPTO'):
+        print
+        print crypto_map.text
+        for child in crypto_map.children:
+            print child.text
+    print
 
-# Find crypto map entries and print children
-for crypto_map in cfg.find_objects(r'^crypto map CRYPTO'):
-    for child in crypto_map.children:
-        print child.text
-
+if __name__ == "__main__":
+    main()
